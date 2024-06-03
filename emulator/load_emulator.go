d4d26549d1fdc5fe89aee0be11b039245d6aafaa
alex538 <alexey.lunkov@gmail.com>
2024-06-04 00:15:06 +0300

    http load emulator

---
+++ emulator/load_emulator.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand/v2"
+	"net/http"
+	"time"
+)
+
+func randRange(min, max int) int {
+	return rand.IntN(max-min) + min
+}
+
+func main() {
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodPut, "http://localhost:8081/processing/create", nil)
+	if err != nil {
+		// handle error
+		log.Fatal(err)
+	}
+
+	for {
+		_, err = client.Do(req)
+		if err != nil {
+			// handle error
+			fmt.Printf(err.Error())
+		}
+		var sleepTimeSec = randRange(10, 1600)
+		var sleepTime = time.Duration(sleepTimeSec) * time.Millisecond
+		time.Sleep(sleepTime)
+	}
+}
